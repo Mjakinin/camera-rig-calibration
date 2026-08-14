8f5efe0d229c7cb1111d7e08128b76a6ca1ac11d
Maxim <125746805+Mjakinin@users.noreply.github.com>
2026-08-14 23:42:54 +0200

    refactor: organize tests and remove obsolete method facades

--- src/camera_rig_calibration/ap01_auto_direct.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility facade for AP01 automatic Direct-target selection."""
-
-from .methods.ap01.automatic_target import automatic_ap01_direct_target
-
-__all__ = ["automatic_ap01_direct_target"]
